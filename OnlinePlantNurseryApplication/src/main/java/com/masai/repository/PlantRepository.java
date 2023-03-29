9b0e54ecc5f9c77d2339bef5d7c41770986f460c
Ayaz Khan <111373057+Ayaz-01@users.noreply.github.com>
2023-03-29 16:21:33 +0530

    service layer for planter and plant completed

--- OnlinePlantNurseryApplication/src/main/java/com/masai/repository/PlantRepository.java
+++ OnlinePlantNurseryApplication/src/main/java/com/masai/repository/PlantRepository.java
@@ -1,5 +1,7 @@
 package com.masai.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.masai.model.Plant;
@@ -7,5 +9,8 @@ import com.masai.model.Plant;
 public interface PlantRepository extends JpaRepository<Plant, Integer> {
 
 	// ayaz khan
+	
+	public Plant findByplantCommonName(String commonName);
+	public List<Plant> findBytypeOfPlant(String typeOfPlant);
 	
 }
--- OnlinePlantNurseryApplication/src/main/java/com/masai/repository/PlanterRepository.java
+++ OnlinePlantNurseryApplication/src/main/java/com/masai/repository/PlanterRepository.java
@@ -1,11 +1,17 @@
 package com.masai.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.masai.model.Planter;
 
 public interface PlanterRepository extends JpaRepository<Planter, Integer> {
 
+
 	// ayaz khan
+	public Planter findByplanterShape(String planterShape);
+	public List<Planter> findByBetweenplanterCost(double minCost, double maxCost)
 	
 }
