6471070d9906e64ea61459b16e53abfc4c6e2d26
shubhamrasam <shubhamrasam5820@gmail.com>
2023-03-29 12:02:06 +0530

    testing

--- OnlinePlantNurseryApplication/src/main/java/com/masai/repository/OrdersRepository.java
+++ OnlinePlantNurseryApplication/src/main/java/com/masai/repository/OrdersRepository.java
@@ -1,6 +1,12 @@
 package com.masai.repository;
 
-public interface OrdersRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.masai.model.Orders;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders, Integer> {
 
 	// shubham rasam
 	
--- OnlinePlantNurseryApplication/src/main/java/com/masai/service/OrdersServiceImpl.java
+++ OnlinePlantNurseryApplication/src/main/java/com/masai/service/OrdersServiceImpl.java
@@ -1,10 +1,68 @@
 package com.masai.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.masai.exception.OrderException;
+import com.masai.model.Orders;
+import com.masai.repository.OrdersRepository;
 
 @Service
 public class OrdersServiceImpl implements OrdersService{
 
-	//Shubham Rasam
+	@Autowired
+	private OrdersRepository ordersRepository;
+	
+	@Override
+	public Orders addOrder(Orders order) throws OrderException {
+		
+		Orders orderObject = ordersRepository.save(order);
+		
+		if(orderObject == null) {
+			
+			throw new OrderException("Order is not added");
+			
+		}
+		
+		return orderObject;
+		
+	}
+
+	@Override
+	public Orders updateOrder(Orders order) throws OrderException {
+		
+		Optional<Orders> orderOpt =  ordersRepository.findById(order.getBookingOrderId());
+		
+		if(orderOpt.isEmpty()) {
+			
+			throw new OrderException("");
+			
+		}
+		
+		return null;
+	}
+
+	@Override
+	public Orders deleteOrder(Integer orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Orders viewOrder(Integer orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Orders> viewAllOrders() throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 	
 }
