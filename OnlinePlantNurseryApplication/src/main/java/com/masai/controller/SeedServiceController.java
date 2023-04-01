00315fd8edfa923167f2fec7f332415ee319e6e7
Ayaz Khan <111373057+Ayaz-01@users.noreply.github.com>
2023-04-01 15:08:41 +0530

    controller for seeds and order updated

--- OnlinePlantNurseryApplication/src/main/java/com/masai/controller/SeedServiceController.java
+++ OnlinePlantNurseryApplication/src/main/java/com/masai/controller/SeedServiceController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masai.exception.LoginException;
@@ -27,51 +28,44 @@ public class SeedServiceController {
 	private SeedService seedService;
 
 
-	@PostMapping("/add/{key}")
-	public ResponseEntity<Seed> addSeed(@RequestBody Seed seed, @PathVariable("key") String key)
-			throws SeedException, LoginException {
+	@PostMapping("/add")
+	public ResponseEntity<Seed> addSeed(@RequestBody Seed seed,@RequestParam(name="key") String key){
 		Seed savedSeed = seedService.addSeed(seed, key);
 		return new ResponseEntity<>(savedSeed, HttpStatus.CREATED);
 	}
 
-	@PutMapping("/update/{key}")
-	public ResponseEntity<Seed> updateSeed(@RequestBody Seed seed, @PathVariable("key") String key)
-			throws SeedException, LoginException {
+	@PutMapping("/update")
+	public ResponseEntity<Seed> updateSeed(@RequestBody Seed seed,@RequestParam(name="key") String key) {
 		Seed updatedSeed = seedService.updateSeed(seed, key);
 		return new ResponseEntity<>(updatedSeed, HttpStatus.OK);
 	}
 
-	@DeleteMapping("/delete/{seedId}/{key}")
-	public ResponseEntity<Seed> deleteSeed(@PathVariable("seedId") Integer seedId, @PathVariable("key") String key)
-			throws SeedException, LoginException {
+	@DeleteMapping("/delete/{seedId}")
+	public ResponseEntity<Seed> deleteSeed(@PathVariable("seedId") Integer seedId,@RequestParam(name="key") String key) {
 		Seed deletedSeed = seedService.deleteSeed(seedId, key);
 		return new ResponseEntity<>(deletedSeed, HttpStatus.OK);
 	}
 
-	@GetMapping("/get/{seedId}/{key}")
-	public ResponseEntity<Seed> viewSeed(@PathVariable("seedId") Integer seedId, @PathVariable("key") String key)
-			throws SeedException, LoginException {
+	@GetMapping("/get/{seedId}")
+	public ResponseEntity<Seed> viewSeed(@PathVariable("seedId") Integer seedId,@RequestParam(name="key") String key){
 		Seed seed = seedService.viewSeed(seedId, key);
 		return new ResponseEntity<>(seed, HttpStatus.OK);
 	}
 
-	@GetMapping("/getByCommon-name/{commonName}/{key}")
-	public ResponseEntity<Seed> viewSeed(@PathVariable("commonName") String commonName, @PathVariable("key") String key)
-			throws SeedException, LoginException {
+	@GetMapping("/getByCommon-name/{commonName}")
+	public ResponseEntity<Seed> viewSeed(@PathVariable("commonName") String commonName,@RequestParam(name="key") String key) {
 		Seed seed = seedService.viewSeed(commonName, key);
 		return new ResponseEntity<>(seed, HttpStatus.OK);
 	}
 
-	@GetMapping("/getAll{key}")
-	public ResponseEntity<List<Seed>> viewAllSeeds(@PathVariable("key") String key)
-			throws SeedException, LoginException {
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Seed>> viewAllSeeds(@RequestParam(name="key") String key) {
 		List<Seed> seeds = seedService.viewAllSeeds(key);
 		return new ResponseEntity<>(seeds, HttpStatus.OK);
 	}
 
-	@GetMapping("/getByType-of-seed/{typeOfSeed}/{key}")
-	public ResponseEntity<List<Seed>> viewAllSeeds(@PathVariable("typeOfSeed") String typeOfSeed,
-			@PathVariable("key") String key) throws SeedException, LoginException {
+	@GetMapping("/getByType-of-seed/{typeOfSeed}")
+	public ResponseEntity<List<Seed>> viewAllSeeds(@PathVariable("typeOfSeed") String typeOfSeed,@RequestParam(name="key") String key){
 		List<Seed> seeds = seedService.viewAllSeeds(typeOfSeed, key);
 		return new ResponseEntity<>(seeds, HttpStatus.OK);
 	}
