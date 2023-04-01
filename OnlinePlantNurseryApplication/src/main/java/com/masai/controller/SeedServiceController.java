f81e83d7a5d0f1206f8d95d9164e1162c78ce517
Ayaz Khan <111373057+Ayaz-01@users.noreply.github.com>
2023-04-01 18:34:55 +0530

    cross-origin dependency added

--- OnlinePlantNurseryApplication/src/main/java/com/masai/controller/SeedServiceController.java
+++ OnlinePlantNurseryApplication/src/main/java/com/masai/controller/SeedServiceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.masai.model.Seed;
 import com.masai.service.SeedService;
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("/seeds")
 public class SeedServiceController {
 
