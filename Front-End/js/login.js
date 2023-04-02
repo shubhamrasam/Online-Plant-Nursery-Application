f46ceec38704555424efa9a53b1a4ee6e0b7db34
Ashish Kumar <ashishbhagat330@gmail.com>
2023-04-02 20:02:24 +0530

    added

--- Front-End/js/login.js
+++ Front-End/js/login.js
@@ -1,1 +1,29 @@
 
+let btn=document.getElementById("btn")
+btn.onclick=()=>{
+    LogIn();
+}
+
+let LogIn=async()=>{
+    try{
+        let custdata={
+            customerMobileNumber:document.querySelector(".mobile-number").value,
+            customerPassword:document.querySelector(".password").value
+        }
+        let res=await fetch(`http://localhost:8088/customer/login`,{
+            method:'POST',
+            body:JSON.stringify(custdata),
+            headers:{
+                'Content-Type':'application/json'
+            }
+           
+        })
+
+       // let data=await res.json()
+        console.log(res.headers)
+        
+
+    }catch(error){
+        console.log("error:",error)
+    }
+}
