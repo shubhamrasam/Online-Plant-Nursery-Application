1355ae624f3f42f33e8f4d5fa3e5daec645e8bc6
Ashish Kumar <ashishbhagat330@gmail.com>
2023-04-03 13:08:27 +0530

    spring security added

--- OnlinePlantNurseryApplication/src/main/java/com/masai/controller/CustomerLoginController.java
+++ OnlinePlantNurseryApplication/src/main/java/com/masai/controller/CustomerLoginController.java
@@ -3,6 +3,7 @@ package com.masai.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +19,12 @@ import com.masai.service.CustomerLoginService;
 public class CustomerLoginController {
 	@Autowired
 	private CustomerLoginService loginService;
-	
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping("/login")
 	public ResponseEntity<String> logInHandler(@RequestBody CustomerLoginDTO dto) {
 		return new ResponseEntity<>(loginService.logIntoAccount(dto),HttpStatus.ACCEPTED);
 	}
-	
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping("/logout")
 	public ResponseEntity<String> logoutInHandler(@RequestParam(required = false) String key) {
 		return new ResponseEntity<>(loginService.logOutAccount(key),HttpStatus.OK);
